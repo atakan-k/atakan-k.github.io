71ddf7b6fa009ca05b6ab353a92793fcdd5a5e96
atakan-k <128419890+atakan-k@users.noreply.github.com>
2025-09-28 13:24:03 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { AppProvider } from './context/AppContext';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Background from './components/Background';
+
+function App() {
+  return (
+    <AppProvider>
+      <div className="text-gray-800 dark:text-gray-200 min-h-screen transition-colors duration-300">
+        <Background />
+        <Header />
+        <main>
+          <Home />
+          <About />
+          <Experience />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </AppProvider>
+  );
+}
+
+export default App;
